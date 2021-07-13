c7b20b1e28c2f8e48395e505364ddcd1a7a45b5b
Harrison Howard <harrison.howard00707@gmail.com>
2021-07-13 13:52:38 +1000

    SingleNode needs a static function to compare two instances of SingleNode

--- src/models/SingleNode.js
+++ src/models/SingleNode.js
@@ -11,4 +11,37 @@ module.exports = class SingleNode {
     get next() {
         return this._next;
     }
+
+    /**
+     * Compare two instances of SingleNode
+     * @param {SingleNode} a 
+     * @param {SingleNode} b 
+     * @returns 
+     */
+    static compareInstance(a, b) {
+        if (typeof a.data !== typeof b.data) {
+            return false;
+        }
+        switch (typeof a.data) {
+            case "object": {
+                for (const [key, value] of Object.entries(a.data)) {
+                    if (a.data[key] !== b.data[key]) {
+                        return false;
+                    }
+                }
+                break;
+            }
+            case "function": {
+                if (a.data.toString() !== b.data.toString()) {
+                    return false;
+                }
+            }
+            default: {
+                if (a.data !== b.data) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 };
