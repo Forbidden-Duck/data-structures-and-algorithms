c6d2c9b640b26b743fdec573bd8022173518755d
Harrison Howard <harrison.howard00707@gmail.com>
2021-07-14 15:11:39 +1000

    Prettier format

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Data Structures and Algorithms
+
 This project is currently under-development.
 
 ## What will it include?
 
 **1. Models of SingleNode and DoubleNode**
+
 - SingleNode is designed to only store the data you include and the next SingleNode instance
 - DoubleNode is similarly designed however it includes the previous DoubleNode instance
 
 **2. SinglyLinkedList and DoublyLinkedList**
+
 - SinglyLinkedList uses SingleNode to create a linked list
 - DoublyLinkedList uses DoubleNode to create a linked list
 
--- src/models/SingleNode.js
+++ src/models/SingleNode.js
@@ -1,59 +1,60 @@
 module.exports = class SingleNode {
-    constructor(data) {
-        this.data = data;
-        /**
-         * @private
-         * @type {SingleNode}
-         */
-        this._next = null;
-    }
-
-    get next() {
-        return this._next;
-    }
-
+  constructor(data) {
+    this.data = data;
     /**
-     * Compare two instances of SingleNode
-     * @param {SingleNode} a 
-     * @param {SingleNode} b 
-     * @returns 
+     * @private
+     * @type {SingleNode}
      */
-    static compareInstance(a, b) {
-        if (!(a instanceof SingleNode)
-            || !(b instanceof SingleNode)) {
-            return null;
-        }
-        if (typeof a.data !== typeof b.data) {
-            return false;
-        }
-        switch (typeof a.data) {
-            case "object": {
-                for (const [key, value] of Object.entries(a.data)) {
-                    if (a.data[key] !== b.data[key]) {
-                        return false;
-                    }
-                }
-                break;
-            }
-            case "function": {
-                if (a.data.toString() !== b.data.toString()) {
-                    return false;
-                }
-            }
-            default: {
-                if (a.data !== b.data) {
-                    return false;
-                }
-            }
-        }
-        if (a.next !== null && b.next !== null) {
-            // Check both instances of next nodes are correct
-            return SingleNode.compareInstance(a._next, b._next);
-        }
-        if ((a.next === null && b.next !== null)
-            || (a.next !== null && b.next === null)) {
-            return false;
-        }
-        return true;
+    this._next = null;
+  }
+
+  get next() {
+    return this._next;
+  }
+
+  /**
+   * Compare two instances of SingleNode
+   * @param {SingleNode} a
+   * @param {SingleNode} b
+   * @returns
+   */
+  static compareInstance(a, b) {
+    if (!(a instanceof SingleNode) || !(b instanceof SingleNode)) {
+      return null;
     }
+    if (typeof a.data !== typeof b.data) {
+      return false;
+    }
+    switch (typeof a.data) {
+      case "object": {
+        for (const [key, value] of Object.entries(a.data)) {
+          if (a.data[key] !== b.data[key]) {
+            return false;
+          }
+        }
+        break;
+      }
+      case "function": {
+        if (a.data.toString() !== b.data.toString()) {
+          return false;
+        }
+      }
+      default: {
+        if (a.data !== b.data) {
+          return false;
+        }
+      }
+    }
+    if (a.next !== null && b.next !== null) {
+      // Check both instances of next nodes are correct
+      return SingleNode.compareInstance(a._next, b._next);
+    }
+    if (
+      (a.next === null && b.next !== null) ||
+      (a.next !== null && b.next === null)
+    ) {
+      return false;
+    }
+    return true;
+  }
 };
