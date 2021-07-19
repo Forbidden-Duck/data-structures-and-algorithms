c67c429697bfb2a9b9a9b74d71ac2b5995cd3d0b
Harrison Howard <harrison.howard00707@gmail.com>
2021-07-19 14:22:13 +1000

    Queue completed (Uses SinglyLinkedList to hold it's elements)

---
+++ src/structures/Queue.js
@@ -0,0 +1,86 @@
+const SinglyLinkedList = require("./SinglyLinkedList");
+const SingleNode = require("../models/SingleNode");
+
+module.exports = class Queue {
+    constructor() {
+        /**
+         * @private
+         */
+        this.elements = new SinglyLinkedList();
+    }
+
+    /**
+     * Get the front of the queue without removing it
+     * @returns {*}
+     */
+    get peek() {
+        return this.elements.head.data;
+    }
+
+    /**
+     * Enqueue the specified node
+     * @param {*} value
+     */
+    enqueue(value) {
+        this.elements.append(new SingleNode(node.data));
+    }
+
+    /**
+     * Remove the front node
+     */
+    dequeue() {
+        if (this.isEmpty()) return null;
+        this.elements.delete(this.elements.head);
+    }
+
+    /**
+     * Returns whether or not the queue is empty
+     * @returns {boolean}
+     */
+    isEmpty() {
+        return this.size <= 0;
+    }
+
+    /**
+     * Remove the entire queue
+     */
+    clear() {
+        this.elements.clear();
+    }
+
+    /**
+     * Create a clone of the queue
+     * @returns {Queue}
+     */
+    clone() {
+        const queue = new Queue();
+        queue.elements = this.elements.clone();
+        return queue;
+    }
+
+    /**
+     * Returns the size of the queue
+     * @returns {number}
+     */
+    get size() {
+        return this.elements.size;
+    }
+
+    /**
+     * Convert the queue into an array of values
+     * @returns {*[]}
+     */
+    toArray() {
+        const values = [];
+        this.elements.forEach((node) => values.push(node.data));
+        return values;
+    }
+
+    /**
+     * Convert the queue into a string of values
+     * @returns {string}
+     */
+    toString() {
+        return this.elements.toString();
+    }
+};
