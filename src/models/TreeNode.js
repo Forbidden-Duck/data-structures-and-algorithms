e9f0742674c3854d163ad8f8d323553b4543af6d
Harrison Howard <harrison.howard00707@gmail.com>
2021-07-20 15:41:52 +1000

    TreeNode created

---
+++ src/models/TreeNode.js
@@ -0,0 +1,47 @@
+module.exports = class TreeNode {
+    constructor(key, data) {
+        /**
+         * @private
+         */
+        this._key = key;
+        this.data = data;
+        /**
+         * @private
+         */
+        this._left = null;
+        /**
+         * @private
+         */
+        this._right = null;
+        /**
+         * @private
+         */
+        this._parent = null;
+    }
+
+    get key() {
+        return this._key;
+    }
+
+    get left() {
+        return this._left;
+    }
+
+    get right() {
+        return this._right;
+    }
+
+    get parent() {
+        return this._parent;
+    }
+
+    /**
+     * Compare two instances of TreeNode
+     * @param {TreeNode} a
+     * @param {TreeNode} b
+     * @param {"left" | "right" | "parent"} skip
+     */
+    static compareInstance(a, b, skip) {
+        throw new Error("Not implemented yet");
+    }
+};
