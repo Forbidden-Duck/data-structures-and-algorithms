fcdff4f2b2f0097356e5e1b1b316cda3e3f2f0be
Harrison Howard <harrison.howard00707@gmail.com>
2021-07-25 17:13:15 +1000

    BT test, use a global instance of BinaryTree

--- test/BinaryTree.test.js
+++ test/BinaryTree.test.js
@@ -1,18 +1,21 @@
 const BinaryTree = require("../src/structures/BinaryTree");
 const TreeNode = require("../src/models/TreeNode");
 
+const BT = new BinaryTree();
+
 describe("BinaryTree", () => {
     describe("constructor", () => {
         it("should create an instance of BinaryTree", () => {
-            expect(new BinaryTree()).toBeInstanceOf(BinaryTree);
+            expect(BT).toBeInstanceOf(BinaryTree);
         });
         it("should set the root property to null", () => {
-            expect(new BinaryTree().root).toBeNull();
+            expect(BT.root).toBeNull();
         });
     });
 
     describe("get root", () => {
         it("should return the set TreeNode", () => {
+            // New instance to not override existing root node
             const tree = new BinaryTree();
             const root = new TreeNode("root");
             tree._root = root;
