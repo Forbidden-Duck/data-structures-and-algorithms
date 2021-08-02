b5b377ca7b4f29a58c7b0274a554094e2c662767
Harrison Howard <harrison.howard00707@gmail.com>
2021-08-02 16:45:11 +1000

    MH constructor test

---
+++ test/MaxHeap.test.js
@@ -0,0 +1,23 @@
+const Heap = require("../src/structures/Heap");
+const MaxHeap = require("../src/structures/MaxHeap");
+const HeapNode = require("../src/models/HeapNode");
+
+const MH = new MaxHeap();
+
+describe("MaxHeap", () => {
+    describe("constructor", () => {
+        it("should not throw an error when being instantiated directly", () => {
+            expect(() => new MaxHeap()).not.toThrow(Error);
+        });
+        it("should create an instance of MaxHeap", () => {
+            expect(MH).not.toBeInstanceOf(Heap);
+            expect(MH).toBeInstanceOf(MaxHeap);
+        });
+        it("should set the nodes property to an empty array", () => {
+            expect(MH.nodes).toStrictEqual([]);
+        });
+        it("should set the root property to null", () => {
+            expect(MH.root).toBeNull();
+        });
+    });
+});
