dd996acab24f21cfa89bef0905685fda380dbab2
Harrison Howard <harrison.howard00707@gmail.com>
2021-08-02 16:36:17 +1000

    H throw Error not TypeError

--- src/structures/Heap.js
+++ src/structures/Heap.js
@@ -5,7 +5,7 @@ const HeapNode = require("../models/HeapNode");
 module.exports = class Heap {
     constructor() {
         if (new.target === Heap)
-            throw new TypeError("Cannot instantiate Heap directly");
+            throw new Error("Cannot instantiate Heap directly");
         /**
          * @private
          * @type {HeapNode[]}
