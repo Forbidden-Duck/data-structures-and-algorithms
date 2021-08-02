e463dd284bc303bf5c0add25b81e2e594a9afa8e
Harrison Howard <harrison.howard00707@gmail.com>
2021-08-02 16:29:28 +1000

    MH compareKeys and clone methods

--- src/structures/MinHeap.js
+++ src/structures/MinHeap.js
@@ -1,4 +1,24 @@
 const Heap = require("./Heap");
 const HeapNode = require("../models/HeapNode");
 
-module.exports = class MinHeap extends Heap {};
+module.exports = class MinHeap extends Heap {
+    /**
+     * Check if the nodes are in a valid position
+     * @param {number} parentKey
+     * @param {number} childKey
+     * @returns {boolean}
+     */
+    compareKeys(parentKey, childKey) {
+        if (isNaN(parentKey)) throw new TypeError("parentKey must be a number");
+        if (isNaN(childKey)) throw new TypeError("childKey must be a number");
+        return parentKey < childKey;
+    }
+
+    /**
+     * Immutably clone the heap and it's nodes
+     * @returns {MinHeap}
+     */
+    clone() {
+        return super.clone(MinHeap);
+    }
+};
