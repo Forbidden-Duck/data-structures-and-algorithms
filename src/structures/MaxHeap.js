d6b6e86bae2f334951496ef6650e0b0ff40a4972
Harrison Howard <harrison.howard00707@gmail.com>
2021-08-02 16:26:17 +1000

    MH compareKeys method

--- src/structures/MaxHeap.js
+++ src/structures/MaxHeap.js
@@ -1,4 +1,16 @@
 const Heap = require("./Heap");
 const HeapNode = require("../models/HeapNode");
 
-module.exports = class MaxHeap extends Heap {};
+module.exports = class MaxHeap extends Heap {
+    /**
+     * Check if the nodes are in a valid position
+     * @param {number} parentKey
+     * @param {number} childKey
+     * @returns {boolean}
+     */
+    compareKeys(parentKey, childKey) {
+        if (isNaN(parentKey)) throw new TypeError("parentKey must be a number");
+        if (isNaN(childKey)) throw new TypeError("childKey must be a number");
+        return parentKey > childKey;
+    }
+};
