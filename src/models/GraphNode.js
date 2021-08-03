bbcd4acf54952c0fcfc317458716f4677b0033e3
Harrison Howard <harrison.howard00707@gmail.com>
2021-08-03 13:11:45 +1000

    GN compareInstance static method

--- src/models/GraphNode.js
+++ src/models/GraphNode.js
@@ -47,4 +47,34 @@ module.exports = class GraphNode {
     deleteEdge(key) {
         this.edges.delete(key);
     }
+
+    /**
+     * Compare two instances of GraphNode
+     * @param {GraphNode} a
+     * @param {GraphNode} b
+     * @returns {boolean}
+     */
+    static compareInstance(a, b) {
+        if (!(a instanceof GraphNode) || !(b instanceof GraphNode)) return null;
+        if (typeof a.data !== typeof b.data) return false;
+        if (a.key !== b.key) return false;
+
+        switch (a.data) {
+            case "object": {
+                for (const [key, value] of Object.entries(a.data)) {
+                    if (a.data[key] !== b.data[key]) return false;
+                }
+                break;
+            }
+            case "function": {
+                if (a.data.toString() !== b.data.toString()) return false;
+                break;
+            }
+            default: {
+                if (a.data !== b.data) return false;
+                break;
+            }
+        }
+        return true;
+    }
 };
