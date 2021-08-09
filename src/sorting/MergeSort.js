4f2f044cc49c79f3f28720c9f452cc713cd7512d
Harrison Howard <harrison.howard00707@gmail.com>
2021-08-09 14:23:37 +1000

    2 days later, the merge sort actually sorts correctly

--- src/sorting/MergeSort.js
+++ src/sorting/MergeSort.js
@@ -2,33 +2,35 @@
  * Takes an array and sorts it using merge sort
  * Does not mutate
  * @template {{key: number, data: *}} T
- * @param {T[]} arr
+ * @param {T[]} originalArr
  * @param {new (key: number, data: *) => T} [classType] Node class used (if non present, assumes items are not nodes)
  * @returns {T[]}
  */
-module.exports = (arr, classType) => {
-    // Step 1: Do not mutate the original array
-    const newArr = [...arr];
+module.exports = (originalArr, classType) => {
+    // Step 1: Extract all the keys from the original array
+    const keyArr = originalArr.map((item) => item.key);
     /**
-     * Step 2: Split the array multiple times
+     * Step 2: Split function
+     * @param {number[]} arr
      * @param {number} start
      * @param {number} end
      */
-    const split = (start, end) => {
+    const split = (arr, start, end) => {
         if (start < end) {
-            const mid = (start + end) / 2;
-            split(start, mid); // Left split
-            split(mid + 1, end); // Right split
-            merge(start, mid, end);
+            const mid = Math.floor((start + end) / 2);
+            split(arr, start, mid); // Left split
+            split(arr, mid + 1, end); // Right split
+            merge(arr, start, mid, end);
         }
     };
     /**
-     * Step 3: Merge the array together
+     * Step 3: Merge function
+     * @param {number[]} arr
      * @param {number} start
      * @param {number} mid
      * @param {number} end
      */
-    const merge = (start, mid, end) => {
+    const merge = (arr, start, mid, end) => {
         // Temp arrays for the left and right
         /**
          * @type {T[]}
@@ -38,64 +40,54 @@ module.exports = (arr, classType) => {
          * @type {T[]}
          */
         const rightArr = [];
-        newArr.forEach((item, index) => {
+        arr.forEach((item, index) => {
             // mid - start + 1 = the size of the left array
-            if (index < mid - start + 1) {
+            if (index >= start && index <= mid) {
                 leftArr.push(item);
-            } else {
+            } else if (index > mid && index <= end) {
                 rightArr.push(item);
             }
         });
 
-        // Merge the arrays with the original
-        let arrayIndex = 0;
+        // Merge the arrays with the temp array
+        let mergeIndex = start;
 
-        // Add the smaller of both iterations of left and right to the array
-        leftArr.forEach((item, index) => {
-            if (!rightArr[index] || item.key <= rightArr[index].key) {
-                if (!classType) {
-                    newArr[arrayIndex] = item;
-                } else {
-                    newArr[arrayIndex] = new classType(item.key, item.data); // Left array item
-                }
-                leftArr.splice(index, 1);
-                arrayIndex++;
+        // Traverse both arrays, for each iteration add the smaller number to the temp array
+        while (leftArr[0] && rightArr[0]) {
+            if (leftArr[0] <= rightArr[0]) {
+                arr[mergeIndex] = leftArr[0];
+                leftArr.shift();
             } else {
-                const rightItem = rightArr[index];
-                if (!classType) {
-                    newArr[arrayIndex] = rightItem;
-                } else {
-                    newArr[arrayIndex] = new classType(
-                        rightItem.key,
-                        rightItem.data
-                    );
-                }
-                rightArr.splice(index, 1);
-                arrayIndex++;
+                arr[mergeIndex] = rightArr[0];
+                rightArr.shift();
             }
-        });
+            mergeIndex++;
+        }
 
-        // Add elements left in the left array
+        // Add the remaining left items
         leftArr.forEach((item) => {
-            if (!classType) {
-                newArr[arrayIndex] = item;
-            } else {
-                newArr[arrayIndex] = new classType(item.key, item.data);
-            }
-            arrayIndex++;
+            arr[mergeIndex] = item;
+            mergeIndex++;
         });
 
-        // Add elements left in the right array
+        // Add the remaining right items
         rightArr.forEach((item) => {
-            if (!classType) {
-                newArr[arrayIndex] = item;
-            } else {
-                newArr[arrayIndex] = new classType(item.key, item.data);
-            }
-            arrayIndex++;
+            arr[mergeIndex] = item;
+            mergeIndex++;
         });
     };
-    // Step 4: Return the sorted array
-    split(0, newArr.length - 1);
-    return newArr;
+    // Step 4: Run the algorithm
+    split(keyArr, 0, keyArr.length - 1);
+    // Step 5: Convert the keys back into nodes
+    const cloneArr = [];
+    keyArr.forEach((key) => {
+        const node = originalArr.find((item) => item.key === key);
+        cloneArr.push(
+            classType
+                ? new classType(node.key, node.data)
+                : Object.assign({}, node)
+        );
+    });
+    // Step 6: Return the clone array
+    return cloneArr;
 };
