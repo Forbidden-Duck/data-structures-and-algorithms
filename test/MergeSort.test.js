e51a68dff8d0cfd05541d6378eb26e11705cc233
Harrison Howard <harrison.howard00707@gmail.com>
2021-08-09 14:30:09 +1000

    MS test

---
+++ test/MergeSort.test.js
@@ -0,0 +1,41 @@
+const MergeSort = require("../src/sorting/MergeSort");
+const HeapNode = require("../src/models/HeapNode");
+const arrayToSort = [
+    new HeapNode(5449, "5449"),
+    new HeapNode(8609, "8609"),
+    new HeapNode(7621, "7621"),
+    new HeapNode(2399, "2399"),
+    new HeapNode(7777, "7777"),
+    new HeapNode(5779, "5779"),
+    new HeapNode(2713, "2713"),
+    new HeapNode(5119, "5119"),
+    new HeapNode(6291, "6291"),
+    new HeapNode(2556, "2556"),
+];
+const sortedArray = [
+    new HeapNode(2399, "2399"),
+    new HeapNode(2556, "2556"),
+    new HeapNode(2713, "2713"),
+    new HeapNode(5119, "5119"),
+    new HeapNode(5449, "5449"),
+    new HeapNode(5779, "5779"),
+    new HeapNode(6291, "6291"),
+    new HeapNode(7621, "7621"),
+    new HeapNode(7777, "7777"),
+    new HeapNode(8609, "8609"),
+];
+
+describe("MergeSort", () => {
+    it("should return the array sorted", () => {
+        const sort = MergeSort(arrayToSort, HeapNode);
+        expect(sort).toStrictEqual(sortedArray);
+    });
+    it("should not affect the original nodes", () => {
+        const sort = MergeSort(sortedArray, HeapNode);
+        sort[0].data = "bad";
+        expect(sort[0].data).toStrictEqual("bad");
+        expect(
+            sortedArray.find((node) => node.key === sort[0].key).data
+        ).not.toStrictEqual("bad");
+    });
+});
